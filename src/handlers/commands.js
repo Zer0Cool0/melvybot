d3abe90cbc02f3796eecd46dd7709d94a6b49a33
Zer0Cool0 <njlewis0093@gmail.com>
2024-07-24 16:41:39 -0400

    Fixed the module imports, switched to rest version 10 (9 is outdated)

--- src/handlers/commands.js
+++ src/handlers/commands.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
-const { PermissionsBitField } = require('discord.js');
-const { Routes } = require('discord-api-types/v9');
-const { REST } = require('@discordjs/rest');
+const { PermissionsBitField, REST, Routes } = require('discord.js');
 const config = require('../config');
 
-const rest = new REST({ version: '9' }).setToken(config.token);
+const rest = new REST({ version: '10' }).setToken(config.token);
 
 module.exports = (client) => {
     const commands = [];
